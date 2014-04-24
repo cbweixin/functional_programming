72e9480b505921c86f15b66d213649edcc335eb0
凌虚御风 <1326906+cbweixin@users.noreply.github.com>
2014-04-23 17:50:18 -0700

    add some expression and function there

--- get_head.hs
+++ get_head.hs
@@ -9,4 +9,17 @@ tell (x:y:_) = "The list has more than two element: " ++ show x ++ "and" ++ show
 length' :: (Num b) => [a] -> b
 length' [] = 0
 length' (_:xs) = 1 + length' xs
+capital :: String -> String
+capital "" = " Empty string "
+capital all@(x:xs) = " The first letter of " ++ all ++ " is " ++ [x]
 
+bmiTell :: ( RealFloat a ) => a-> a-> String
+bmiTell weight height
+    | weight / height ^ 2 <= 18.5 = "You're underweight"
+    | weight / height ^ 2 <= 25.5 = "You're normal"
+    | weight / height ^ 2 <= 30.5 = "You're fat "
+    | otherwise = " you are a whale, cong "
+
+calcBmis :: ( RealFloat a ) => [(a,a)] -> [a]
+calcBmis xs = [bmi w h | (w,h) <-xs]
+    where bmi weight height = weight / height ^ 2
